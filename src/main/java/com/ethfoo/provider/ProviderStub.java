25e9bd28c7875e4bdce0f194b798206d3ab74609
ethfoo <ethfoo@gmail.com>
2015-12-01 19:15:31 +0800

    init

---
+++ src/main/java/com/ethfoo/provider/ProviderStub.java
@@ -0,0 +1,5 @@
+package com.ethfoo.provider;
+
+public class ProviderStub {
+
+}
---
+++ src/test/java/felix/Provider.java
@@ -0,0 +1,10 @@
+package felix;
+
+public class Provider {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
